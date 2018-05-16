4983bd56e7aaacd6f1f360c26b25dd9b7328e4dc
Jason Price <jprice@airsquirrels.com>
2018-05-16 14:20:37 -0400

    Updating webpack configs

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const DirectoryNamedWebpackPlugin = require("directory-named-webpack-plugin");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
     entry: path.join(__dirname, 'index.js'),
     output: {
@@ -19,6 +20,31 @@ module.exports = {
                     }
                 }
             },
+            {
+                test: /\.less$/,
+                use: [
+                    "style-loader",
+                    {
+                        loader: "css-loader",
+                        options: {
+                            
+                            modules: true,
+                            localIdentName: '[name]_[local]--[hash:base64:5]'
+                        }
+                    },
+                    {
+                        loader: "less-loader"
+                    }
+                ]
+            },
+            {
+                test: /\.(jpe?g|png|gif)$/,
+                loader: 'file-loader',
+                options: {
+                    name: '[path][name].[ext]'
+                }
+            },
+
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
--- webpack.config.lib.js
+++ webpack.config.lib.js
@@ -2,8 +2,9 @@ const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const DirectoryNamedWebpackPlugin = require("directory-named-webpack-plugin");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-
+const base = require('./webpack.config');
 module.exports = {
+    ...base,
     entry: path.join(__dirname, 'index.js'),
     output: {
         filename: 'lib/bundle.js',
@@ -18,33 +19,5 @@ module.exports = {
             amd: 'react'
           }
         }
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use:{
-                    loader: 'css-loader',
-                    options: {
-                        modules: true,
-                        localIdentName: '[name]_[local]--[hash:base64:5]'
-                    }
-                }
-            },
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env'],
-                        plugins: ['transform-react-jsx', 'react-css-modules']
-                    }
-              }
-            }
-        ]
-    },
-    plugins: [
-      new HtmlWebpackPlugin()
     ]
 }
